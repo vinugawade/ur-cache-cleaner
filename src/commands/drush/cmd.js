9617ec3c5461ba9d580b737c77403e46fd695c76
vinay-osseed <vinay@osseed.com>
2023-01-11 15:25:11 +0530

    feat: add clear cache support for `ddev` project environment

--- src/commands/drush/cmd.js
+++ src/commands/drush/cmd.js
@@ -1,49 +1,76 @@
 const vscode = require('vscode');
 const cmdRunner = require('child_process');
+const fs = require('fs');
 
-// Drush commands.
+// Drush settings.
 const title = "Drush";
 const name = "drush";
 const root = vscode.workspace.rootPath;
-const path = root + '/vendor/drush/drush';
-const version = path + '/' + name + ' version';
-const cr = path + '/' + name + ' cr';
+const version = name + ' version';
 const successMsg = "[success] Cache rebuild complete.";
+var cacheRebuild = "drush > /dev/null";
 
 /**
  * @param {object} extStatusBarItem
  * @param {string} drushVersion
  */
 function clearCache(extStatusBarItem, drushVersion) {
-	cmdRunner.exec(cr, (exps, stdout, stderr) => {
+
+	// Modify command if it's running in ddev environment.
+	cacheRebuild = checkDdevEnv(cacheRebuild);
+
+	cmdRunner.exec(cacheRebuild, (exps, stdout, stderr) => {
 		if (stderr) {
+			// Show success output & update values of status bar button.
 			let output = stderr.trim();
-			if (output.includes(successMsg)) {
+			if (output.includes('[success]')) {
 				vscode.window.showInformationMessage(successMsg);
 				console.log(successMsg);
-
-				extStatusBarItem.text = `$(clear-cache)  Clear Cache`;
-				extStatusBarItem.tooltip = `Drupal (${title} ${drushVersion})`;
 			} else {
-				vscode.window.showErrorMessage('${title} error: ' + stderr);
+				vscode.window.showErrorMessage(`${title} error: ` + stderr);
 				console.log(stderr);
 			}
+			extStatusBarItem.text = `$(clear-cache)  Clear Cache`;
+			extStatusBarItem.tooltip = `Drupal (${title} ${drushVersion})`;
 		}
+
 		if (exps) {
+			// Show exception & update values of status bar button.
 			vscode.window.showErrorMessage('Exception: ' + exps);
 			console.log(exps);
+			extStatusBarItem.text = `$(clear-cache)  Clear Cache`;
+			extStatusBarItem.tooltip = `Drupal (${title} ${drushVersion})`;
 		}
+
 		if (stdout) {
+			// Show output & update values of status bar button.
 			vscode.window.showInformationMessage('stdout: ' + stdout);
 			console.log(stdout);
+			extStatusBarItem.text = `$(clear-cache)  Clear Cache`;
+			extStatusBarItem.tooltip = `Drupal (${title} ${drushVersion})`;
 		}
 	});
+}
+
+/**
+ * @param {string} cacheRebuildCmd
+ */
+function checkDdevEnv(cacheRebuildCmd) {
+	// Check .ddev directory exist or not in workspace.
+	var ddev_config = root + '/.ddev'
+	if (fs.existsSync(ddev_config)) {
+		cacheRebuildCmd = `ddev ` + name + ` cr`;
+	} else {
+		cacheRebuildCmd = name + ' cr --root=' + root;
+	}
+	return cacheRebuildCmd;
 }
 
 module.exports = {
 	title,
 	name,
-	path,
+	root,
 	version,
 	clearCache,
+	checkDdevEnv
 }
