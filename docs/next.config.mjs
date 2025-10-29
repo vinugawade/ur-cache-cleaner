c474597e69fab76e9fc025b6c074aec4f4714c30
vinay-osseed <vinay@osseed.com>
2025-10-30 00:23:52 +0530

    feat: Update Next.js configuration and package.json for static export support

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    
+  output: 'export',              // Required for static export
+  images: { unoptimized: true }, // GitHub Pages doesn’t support Next.js Image Optimization
+  basePath: isProd ? '/ur-cache-cleaner' : '',  // Repo name here
+  assetPrefix: isProd ? '/ur-cache-cleaner/' : '',
 };
 
 export default nextConfig;
